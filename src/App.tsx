3a0057f68d691ff4526e3d231cb1933230bc0e81
John Fulkerson <johnfulky@mac.com>
2023-01-22 16:19:12 -0500

    fully operational

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,16 @@ import useHotkeys from '@reecelucas/react-use-hotkeys';
 type UnitOfTimeT = 'MINUTES' | 'SECONDS';
 
 function UnitOfTime(props: {
-  value: number | string;
+  value: number;
+  typing: string;
   name: UnitOfTimeT;
   setActive: (name: UnitOfTimeT) => void;
   active?: UnitOfTimeT;
 }) {
+  const value =
+    props.name === props.active && props.typing.length > 0
+      ? props.typing
+      : props.value;
   return (
     <span
       className={classnames('cursor-pointer', {
@@ -17,7 +22,7 @@ function UnitOfTime(props: {
       })}
       onClick={() => props.setActive(props.name)}
     >
-      {`00${props.value}`.slice(-2)}
+      {`00${value}`.slice(-2)}
     </span>
   );
 }
@@ -26,6 +31,7 @@ function App() {
   const [isRunning, setRunning] = useState(false);
   const [timer, setTimer] = useState<[number, number]>([0, 0]);
   const [initTimer, setInitTimer] = useState<[number, number]>([0, 0]);
+  const [stoppedTimer, setStoppedTimer] = useState<[number, number]>([0, 0]);
   const [activeUnitOfTime, setActiveUnitOfTime] = useState<
     UnitOfTimeT | undefined
   >();
@@ -71,23 +77,47 @@ function App() {
     setRunning(false);
     setTimer(initTimer);
   });
+  useHotkeys('Escape', () => {
+    if (activeUnitOfTime) {
+      setTyping(['', '']);
+      setTimer(stoppedTimer);
+      setActiveUnitOfTime(undefined);
+    }
+  });
+  useHotkeys('Enter', () => {
+    if (activeUnitOfTime) {
+      setActiveUnitOfTime(undefined);
+      if (activeUnitOfTime === 'MINUTES') {
+        const newValue =
+          typing[0].length === 0 ? timer[0] : parseInt(typing[0]);
+        setTimer([newValue, timer[1]]);
+      }
+      if (activeUnitOfTime === 'SECONDS') {
+        const newValue =
+          typing[1].length === 0 ? timer[1] : parseInt(typing[1]);
+        setTimer([timer[0], newValue]);
+      }
+      setTyping(['', '']);
+    }
+  });
+
   useHotkeys(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], (event) => {
     if (activeUnitOfTime) {
       const currentValue =
         activeUnitOfTime === 'MINUTES' ? typing[0] : typing[1];
       const newValue = `${currentValue}${event.key}`.slice(-2);
-      console.log(`[${newValue}]`)
       if (activeUnitOfTime === 'MINUTES') {
         setTyping([newValue, typing[1]]);
         if (newValue.length === 2) {
+          setTimer([parseInt(newValue), timer[1]]);
           setActiveUnitOfTime('SECONDS');
         }
       } else {
         setTyping([typing[0], newValue]);
         if (newValue.length === 2) {
           setActiveUnitOfTime(undefined);
-          setTimer([parseInt(typing[0]), parseInt(newValue)]);
-          setInitTimer([parseInt(typing[0]), parseInt(newValue)]);
+          setTimer([timer[0], parseInt(newValue)]);
+          setInitTimer([timer[0], parseInt(newValue)]);
           setTyping(['', '']);
         }
       }
@@ -118,17 +148,25 @@ function App() {
     <div className="flex flex-col items-center justify-center h-full gap-[2vw]">
       <p className="text-white font-bold text-[22vw]">
         <UnitOfTime
-          value={activeUnitOfTime ? typing[0] : timer[0]}
+          value={timer[0]}
+          typing={typing[0]}
           name="MINUTES"
           active={activeUnitOfTime}
-          setActive={(newActive: UnitOfTimeT) => setActiveUnitOfTime(newActive)}
+          setActive={(newActive: UnitOfTimeT) => {
+            setActiveUnitOfTime(newActive);
+            setStoppedTimer(timer);
+          }}
         />
         :
         <UnitOfTime
-          value={activeUnitOfTime ? typing[1] : timer[1]}
+          value={timer[1]}
+          typing={typing[1]}
           name="SECONDS"
           active={activeUnitOfTime}
-          setActive={(newActive: UnitOfTimeT) => setActiveUnitOfTime(newActive)}
+          setActive={(newActive: UnitOfTimeT) => {
+            setActiveUnitOfTime(newActive);
+            setStoppedTimer(timer);
+          }}
         />
       </p>
       <div className="flex flex-row gap-[2vw]">
