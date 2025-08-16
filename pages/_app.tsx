25d2d540073be6b6790fd0a701c92ab95f45b255
John Fulkerson <johnfulky@mac.com>
2025-08-16 17:03:52 -0600

    added analytics and dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../src/styles/input.css';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
